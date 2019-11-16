a9eb3f649f165a80aafe3300cd26d4670fff4711
ChenKB <bensoncat99@gmail.com>
2019-11-17 00:29:46 +0800

    new todo stuff

---
+++ change_code.py
@@ -0,0 +1,17 @@
+# TODO:
+# Backup original code
+# reset function
+# read config file
+# change rng
+# change foil generatinon method
+#
+
+def read_config(config_file):
+    pass
+
+def set_rng(rng):
+    pass
+
+def set_generation_method(method):
+    pass
+
